58fcdfc295b31a2c437f9a1de65281c1955dbb17
DieFerre12 <164224192+DieFerre12@users.noreply.github.com>
2024-09-28 14:07:57 -0300

    Add files via upload

---
+++ clase1/FacturaHasMap.java
@@ -0,0 +1,16 @@
+package progra3.clase1;
+import java.util.HashMap;
+
+public class FacturaHasMap {
+    public static void main(String[] args) {
+        // un map es un diccionario, TAD  diccionario<Clave,Valor> 
+        // invariante de representación: la clave no tiene duplicados
+        HashMap<Integer,String> capitulos = new HashMap<>();
+        capitulos.put(100,"Capitulo 100");
+        capitulos.put(101, "Capitulo 101");
+        System.out.println(capitulos.get(101));
+        for(Integer i: capitulos.keySet()) {
+            System.out.println(capitulos.get(i));
+        }
+    }
+}
